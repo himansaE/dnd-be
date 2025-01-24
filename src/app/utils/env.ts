5ad61604ea66c4101dadf6609c1b49324d2c384d
Himansa Eshan <41321171+himansaE@users.noreply.github.com>
2025-01-25 04:00:05 +0530

    add dotenv and zod for environment variable management; implement validation and retrieval functions

---
+++ src/app/utils/env.ts
@@ -0,0 +1,61 @@
+import { env } from "hono/adapter";
+import type { Context } from "hono";
+import { z } from "zod";
+import { config } from "dotenv";
+
+// Initialize dotenv
+config();
+
+const envSchema = z.object({
+  PORT: z.string(),
+  DATABASE_URL: z.string(),
+});
+
+type Env = z.infer<typeof envSchema>;
+
+function formatZodError(error: z.ZodError): string {
+  return error.issues
+    .map((issue) => {
+      const path = issue.path.join(".");
+      return `❌ ${path}: ${issue.message}
+Required environment variable ${path} is ${
+        process.env[path] === undefined ? "missing" : "invalid"
+      }
+Expected type: ${(issue as any).expected || "unknown"}
+Received: ${
+        process.env[path] === undefined ? "undefined" : typeof process.env[path]
+      }`;
+    })
+    .join("\n\n");
+}
+
+export function validateEnvs() {
+  try {
+    const parsedEnvs = envSchema.safeParse(process.env);
+
+    if (!parsedEnvs.success) {
+      const formattedError = formatZodError(parsedEnvs.error);
+      console.error("\n🚨 Environment Variables Validation Error:\n");
+      console.error(formattedError);
+      console.error(
+        "\nPlease check your .env file and provide all required variables.\n"
+      );
+      return false;
+    }
+
+    return true;
+  } catch (error) {
+    console.error(
+      "\n🚨 Unexpected error while validating environment variables"
+    );
+    return false;
+  }
+}
+
+// this is a helper function to get the environment variable
+export function getEnvVariable<K extends keyof Env>(
+  key: K,
+  defaultValue: Env[K]
+): Env[K] {
+  return process.env[key] ?? defaultValue;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,20 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+import { getEnvVariable, validateEnvs } from "./app/utils/env.js";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+// validate the environment variables
+if (!validateEnvs()) process.exit(1);
 
-const port = 3000
-console.log(`Server is running on http://localhost:${port}`)
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
+
+const port = Number(getEnvVariable("PORT", "3000"));
+console.log(`Server is running on http://localhost:${port}`);
 
 serve({
   fetch: app.fetch,
-  port
-})
+  port,
+});
