909c78d292f96c1bf93778a2d52cde4aae8e98f8
Himansa Eshan <41321171+himansaE@users.noreply.github.com>
2025-01-26 03:43:29 +0530

    add authentication middleware and logging; enforce user authentication on root route

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,19 @@
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
 import { getEnvVariable, validateEnvs } from "./app/utils/env.js";
+import { clerkMiddleware } from "@hono/clerk-auth";
+import { authRequired } from "./app/middleware/auth.middleware.js";
+import { logger } from "hono/logger";
 
 const app = new Hono();
 
 // validate the environment variables
 if (!validateEnvs()) process.exit(1);
 
-app.get("/", (c) => {
+app.use("*", clerkMiddleware());
+app.use("*", logger());
+
+app.get("/", authRequired, (c) => {
   return c.text("Hello Hono!");
 });
 
